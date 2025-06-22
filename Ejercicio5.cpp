f4ca8e740259ed4a77fcc4377493d250b97bd6fd
ShirleyCauna <caunashirley11@gmail.com>
2025-06-22 18:16:11 -0500

    Ejercicio 5 - version final: ya cuenta el mayor, menor y cuántas veces se repiten

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -4,6 +4,41 @@
 using namespace std;
 
 int main(){
-    
+    const int MAX_ELEMENTOS = 100;
+    int arregloNumeros[MAX_ELEMENTOS];
+    int cantidadElementos;
+
+    cout << "Ingrese la cantidad total de elementos que desea procesar: ";
+    cin >> cantidadElementos;
+
+    for (int indice = 0; indice < cantidadElementos; indice++) {
+        cout << "Ingrese el valor del elemento #" << indice + 1 << ": ";
+        cin >> arregloNumeros[indice];
+    }
+
+    int numeroMayor = arregloNumeros[0];
+    int numeroMenor = arregloNumeros[0];
+    int repeticionesMayor = 1;
+    int repeticionesMenor = 1;
+
+    for (int i = 1; i < cantidadElementos; i++) {
+        if (arregloNumeros[i] > numeroMayor) {
+            numeroMayor = arregloNumeros[i];
+            repeticionesMayor = 1;
+        } else if (arregloNumeros[i] == numeroMayor) {
+            repeticionesMayor++;
+        }
+
+        if (arregloNumeros[i] < numeroMenor) {
+            numeroMenor = arregloNumeros[i];
+            repeticionesMenor = 1;
+        } else if (arregloNumeros[i] == numeroMenor) {
+            repeticionesMenor++;
+        }
+    }
+
+    cout << "\nNúmero mayor encontrado: " << numeroMayor << " (ocurre " << repeticionesMayor << " veces)" << endl;
+    cout << "Número menor encontrado: " << numeroMenor << " (ocurre " << repeticionesMenor << " veces)" << endl;
+
     return 0;
 }
