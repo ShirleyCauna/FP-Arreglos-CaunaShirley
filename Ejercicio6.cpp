868d2add544c88a1126ebd86143cfcfb7eded178
ShirleyCauna <caunashirley11@gmail.com>
2025-06-22 18:40:02 -0500

    Ejercicio 6 – avance 2: cálculo de suma en índices pares e impares

--- Ejercicio6.cpp
+++ Ejercicio6.cpp
@@ -13,6 +13,22 @@ int main() {
         cout << "Elemento " << i << ": ";
         cin >> vector[i];
     }
+    
+    // === AQUÍ comienza el avance 2 ===
+    int sumaPar = 0, sumaImpar = 0;
+
+    for (i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            sumaPar += vector[i];
+        } else {
+            sumaImpar += vector[i];
+        }
+    }
+
+    cout << "Suma de componentes en posiciones pares: " << sumaPar << endl;
+    cout << "Suma de componentes en posiciones impares: " << sumaImpar << endl;
+    
+    // === Fin del avance 2 ===
 
     return 0;
 }
