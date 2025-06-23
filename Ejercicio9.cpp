62aaa798352163aee2f3f789fbd2ac7c1922ee0b
ShirleyCauna <caunashirley11@gmail.com>
2025-06-22 20:26:06 -0500

    Ejercicio 9 – versión final: rotación del vector completada

--- Ejercicio9.cpp
+++ Ejercicio9.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 	int n;
-	cout << "TamaÃ±o del vector: ";
+	cout << "Tamano del vector: ";
 	cin >> n;
 
 	int Num[n];
@@ -13,6 +13,24 @@ int main(){
 		cout << "Dato " << i << ": ";
 		cin >> Num[i];
 	}
+	
+	// Guardar el último dato
+	int ult = Num[n - 1];
+
+	// Mover los demás hacia la derecha
+	for(int i = n - 1; i > 0; i--){
+		Num[i] = Num[i - 1];
+	}
+
+	// El último va al inicio
+	Num[0] = ult;
+
+	cout << "\nVector rotado una posición a la derecha:" << endl;
+	for(int i = 0; i < n; i++){
+		cout << Num[i] << " ";
+	}
+
+	cout << endl;
 
 	return 0;
 }
