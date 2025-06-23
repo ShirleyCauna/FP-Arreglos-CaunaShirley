c85b0125068c4009a729392baebde37d443e7149
ShirleyCauna <caunashirley11@gmail.com>
2025-06-23 00:28:36 -0500

    Ejercicio 3 – versión final: contar ocurrencias de una letra

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-using namespace std;
+//Contar la ocurrencia de una letra especifica en una cadena de entrada.
 
 #include <iostream>
 using namespace std;
@@ -7,6 +6,7 @@ using namespace std;
 int main(){
     char palabra[30];
     char letra[2];
+    int contador = 0;
 
     cout << "Ingrese la palabra: ";
     cin.getline(palabra, 30);
@@ -14,5 +14,12 @@ int main(){
     cout << "Indique qué letra deseas contar: ";
     cin >> letra;
 
+    for (int i = 0; palabra[i] != '\0'; i++) {
+        if (palabra[i] == letra[0]) {
+            contador++;
+        }
+    }
+
+    cout << "La letra '" << letra[0] << "' aparece " << contador << " veces." << endl;
     return 0;
 }
