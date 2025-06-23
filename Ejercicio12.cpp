3ac4b5b685f4970e65542a5212bacba1ccf13742
ShirleyCauna <caunashirley11@gmail.com>
2025-06-23 01:28:37 -0500

    Ejercicio 12 – versión final: verificación de orden implementada

--- Ejercicio12.cpp
+++ Ejercicio12.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 using namespace std;
 
+// Funcion que verifica si el vector esta ordenado
+bool ordenado(int v[], int tam){
+	for(int i = 0; i < tam - 1; i++){
+		if(v[i] > v[i + 1]){
+			return false;
+		}
+	}
+	return true;
+}
+
 int main(){
 	int n;
 	int vec[100];
@@ -13,6 +23,12 @@ int main(){
 	cout << "Ingresa los elementos:" << endl;
 	for(int i = 0; i < n; i++){
 		cin >> vec[i];
+	}
+
+	if(ordenado(vec, n)){
+		cout << "El vector esta ordenado." << endl;
+	} else {
+		cout << "El vector no esta ordenado." << endl;
 	}
 
 	return 0;
