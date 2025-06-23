a2e989f504cceb14157ede12d5b63e92a0e376d4
ShirleyCauna <caunashirley11@gmail.com>
2025-06-23 03:09:54 -0500

    Ejercicio 15 – avance 1: ingreso de cadenas y estructura de función

---
+++ Ejercicio15.cpp
@@ -0,0 +1,25 @@
+// Generar la funcion mistrcmp(str1,str2) que compara dos cadenas y retorna los resultados 0 si las cadenas son iguales, 1 si la cadena str1 >str2, -1 si la cadena str2>str1. No usar las funciones de String.h
+
+#include <iostream>
+using namespace std;
+
+// Estructura de la funcion, aun no usada
+int mistrcmp(char str1[], char str2[]) {
+    int i = 0;
+    // Logica se implementara despues
+    return 0;
+}
+
+int main() {
+    char str1[50];
+    char str2[50];
+
+    cout << "Ingrese la primera cadena: ";
+    cin >> str1;
+
+    cout << "Ingrese la segunda cadena: ";
+    cin >> str2;
+
+
+    return 0;
+}
