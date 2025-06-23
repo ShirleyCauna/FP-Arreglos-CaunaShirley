88da8477c4bb187b2cacec6e6afb67eca1941689
ShirleyCauna <caunashirley11@gmail.com>
2025-06-23 01:55:27 -0500

    Ejercicio 13 – avance 1: definición de mistrcpy

---
+++ Ejercicio13.cpp
@@ -0,0 +1,21 @@
+// Generar la función mistrcpy(str1,str2) que copia str2 en str1. No usar la funciones de String.h
+
+#include<iostream>
+using namespace std;
+
+void mistrcpy(char str1[], char str2[]){
+	int i = 0;
+	while(str2[i] != '\0'){
+		str1[i] = str2[i];
+		i++;
+	}
+	str1[i] = '\0';
+}
+
+int main() {
+	char str1[20];
+	char str2[] = "programar en C++"; 
+
+
+	return 0;
+}
