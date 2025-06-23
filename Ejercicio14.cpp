85d151814649f0392d0b67bc816ea099da88d210
ShirleyCauna <caunashirley11@gmail.com>
2025-06-23 02:34:36 -0500

    Ejercicio 14 – avance 1: función mistrcat construida

---
+++ Ejercicio14.cpp
@@ -0,0 +1,25 @@
+// Generar la funcion mistrcat(str1,str2) que agrega str2 al final de str1. No usar las funciones de String.h.
+
+#include<iostream>
+using namespace std;
+
+void mistrcat(char str1[], char str2[]){
+	int i = 0;
+	while(str1[i] != '\0'){
+		i++;
+	}
+	int j = 0;
+	while(str2[j] != '\0'){
+		str1[i] = str2[j];
+		i++;
+		j++;
+	}
+	str1[i] = '\0'; // Fin de cadena
+}
+
+int main(){
+	char str1[100] = "Bienvenido ";
+	char str2[] = "a programacion";
+
+	return 0;
+}
