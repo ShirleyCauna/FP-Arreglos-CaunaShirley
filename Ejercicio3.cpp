9f53cc14de5151ec532a985a0eb609a813b22cb6
ShirleyCauna <caunashirley11@gmail.com>
2025-06-23 00:19:29 -0500

    Ejercicio 3 – avance 1: lectura de palabra y letra

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
 int main(){
-	char palabra[30];
-	char letra[2];
-	int a = 0;
-	cout << "Ingrese la palabra: ";
-	cin.getline(palabra,30);
-	cout<<"Indique que letra deseas saber su recurrencia: ";
-	cin >> letra;
-	
-	for (int i=0;i<=30;i++){
-		if (palabra[i]==letra[0]){
-			a = a + 1;
-		}
-	}
-	cout<<"La recurrencia de la letra es : "<<a;
-	return 0;
+    char palabra[30];
+    char letra[2];
+
+    cout << "Ingrese la palabra: ";
+    cin.getline(palabra, 30);
+
+    cout << "Indique qué letra deseas contar: ";
+    cin >> letra;
+
+    return 0;
 }
